f48580a8c854d741401e10f2bed1fbc162cd88ef
Alex McDaniel <amcdaniel186@gmail.com>
2019-02-14 14:55:16 -0500

    initupdate

---
+++ variables.php
@@ -0,0 +1,8 @@
+<!-- to write in php, you must surround the code in php tags -->
+<!-- can wrap the php code in html tags, like <h1> <p> etc, it'll alter the code accordingly. -->
+
+
+<?php
+  echo 'Hello World';
+?>
+<!-- echo returns no value, print returns a value of 1 as well as printing information like echo does. -->
